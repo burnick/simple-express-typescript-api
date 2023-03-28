d569302cfaf1de847c7a823840bfbf50ddd5eed6
burnick <arnelceledonio@gmail.com>
2023-03-28 13:48:36 +0800

    fix: checing default route

--- src/routes/DefaultRoute.test.ts
+++ src/routes/DefaultRoute.test.ts
@@ -6,6 +6,10 @@ describe('Message Endpoint Unit tests for Default Root', () => {
   it(`Message Endpoint check message: Welcome to default api route`, async () => {
     const res = await request(server).get('/');
     expect(res).to.have.property('status', 200);
+    expect(res.header).to.have.property(
+      'content-type',
+      'application/json; charset=utf-8'
+    );
     expect(res.body).to.have.property(
       'message',
       'Welcome to default api route'
